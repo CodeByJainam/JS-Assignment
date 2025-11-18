da01fdfeb7ce4c35247cc708b8dd53e88a73cfc2
jainamsathwara24 <jainamsathwara04@gmail.com>
2025-11-18 11:22:00 +0530

    Project-2

---
+++ Condition with Looping Programs/index.js
@@ -0,0 +1,25 @@
+
+
+let str = "Jainam"
+console.log(str)
+let strength = "strong"
+console.log(strength)
+
+let userName = "Jainam"
+userName = "ABC"
+userName = "XYZ"
+console.log(userName)
+
+let myName = "Jainam"
+let age = 22
+console.log("My name is " + myName + " and my age is " + age + ".")
+
+let jainam = {
+    myNaame: "Jainam",
+    school: "Shiv Ashish School",
+    grade: "A",
+    section: "A",
+    rollNo: 43,
+    marks: [80, 70, 90]
+}
+console.log(jainam)
